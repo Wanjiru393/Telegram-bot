84dec9c2d443169ead3c4fd7d725b06e829c5e0a
Wanjiru393 <carolciru393@gmail.com>
2023-05-07 00:19:46 +0300

    Bot successful

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a Python script using the Telegram Bot API and the Requests library to create a bot that sends random pictures to users who interact with it.
+
+
---
+++ bot.py
@@ -0,0 +1,35 @@
+from telegram import *
+from telegram.ext import *
+from requests import *
+
+updater = Updater(token="5813496849:AAHOcg_n2DMoR7JntX6pzDjCO6pNZKb7Arg")
+dispatcher = updater.dispatcher
+
+randomPictureText = "Random Picture"
+randomPersonText = "Static Image"
+
+randomPictureUrl = "https://picsum.photos/1200"
+randomPersonUrl = "https://picsum.photos/seed/picsum/200/300"
+
+
+def start(update: Update, context:CallbackContext):
+    buttons = [[KeyboardButton(randomPictureText)], [KeyboardButton(randomPersonText)]]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to LaFliq Gallery!",
+    reply_markup=ReplyKeyboardMarkup(buttons))
+
+def messageHandler(update: Update, context: CallbackContext):
+    if randomPersonText in update.message.text:
+        image = get(randomPersonUrl).content
+    if randomPictureText in update.message.text:
+        image = get(randomPictureUrl).content
+    
+    if image:
+        context.bot.sendMediaGroup(chat_id=update.effective_chat.id, media=[InputMediaPhoto
+        (image, caption="")])
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(MessageHandler(Filters.text, messageHandler))
+
+
+updater.start_polling()
+updater.idle()
